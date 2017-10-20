de0058a0bc37aa0eea823a6fb89976f74070e578
rajalparmar <32847646+rajalparmar@users.noreply.github.com>
2017-10-20 18:24:29 -0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     Rigidbody2D rb2d;
+    float speed;	
 
     // Use this for initialization
     void Start () {
-		
+		rb2d = GetComponent<Rigidbody>();
     }
 	
     // Update is called once per frame
     void Update () {
-		
+		float move_Horizontal = Input.GetAxis("Horizontal"); 
+		float move_Vertical = Input.GetAxis("Vertical");
+	    	Vector3 movement = new Vector3(move_Horizontal, 0.0f, move_Vertical);
+	    	rb2d.AddForce(movement * speed);
     }
 }
