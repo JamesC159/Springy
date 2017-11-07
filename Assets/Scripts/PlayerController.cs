0199f99ff7c992011a411bef82fb9e664fa4beab
James Combs <combs.james1993@gmail.com>
2017-11-07 14:16:37 -0600

    Spring physics implemented by Conservation of Energy equations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    Rigidbody2D rb2d;
-    float speed;	
+    private Vector2 vel;
+    private Rigidbody2D rb2d;
 
     // Use this for initialization
-    void Start () {
-		rb2d = GetComponent<Rigidbody>();
+    private void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
     }
-	
+
     // Update is called once per frame
-    void Update () {
-		float move_Horizontal = Input.GetAxis("Horizontal"); 
-		float move_Vertical = Input.GetAxis("Vertical");
-	    	Vector2 movement = new Vector2(move_Horizontal, move_Vertical);
-	    	rb2d.AddForce(movement * speed);
+    private void Update () {
+        if (rb2d.velocity.sqrMagnitude > 0) {
+            vel = rb2d.velocity;
+        }
+    }
+
+    private void FixedUpdate() {
+        
+    }
+
+    public Vector2 GetVel() {
+        return vel;
     }
 }
--- Assets/Scripts/SpringController.cs
+++ Assets/Scripts/SpringController.cs
@@ -3,43 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpringController : MonoBehaviour {
-    
-    GameObject player;
-    Rigidbody2D rb2d;
-    float k;
-    float distancePulled;
-    float launchForce;
 
-    void Awake () {
-        player = GameObject.FindGameObjectWithTag ("Player");
+    public float k;
+    public float draggingMass;
+
+    private Rigidbody2D rb2d;
+    private Rigidbody2D ballRb;
+    private Vector3 initialPos;
+    private Vector2 ballVel;
+    private float x;
+    private bool dragging;
+
+    private void Awake () {
         rb2d = GetComponent<Rigidbody2D> ();
+        ballRb = null;
     }
 
     // Use this for initialization
-    void Start () {
-
+    private void Start () {
+        
     }
 	
     // Update is called once per frame
-    void Update () {
-		
+    private void Update () {
+       
     }
 
-    void OnCollisionEnter2D (Collision2D c) {
-        Rigidbody2D obj = c.collider.attachedRigidbody;
-        if (obj.Equals (player.GetComponent<Rigidbody2D> ())) {
-            // Use this to make ball bounce	
+    private void FixedUpdate() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            ballRb = collision.collider.gameObject.GetComponent<Rigidbody2D>();
+            PlayerController p = ballRb.gameObject.GetComponent<PlayerController>();
+            // Make the ball's rigidbody kinematic so that we can apply Conservation of Energy
+            // equations for this spring system.
+            ballRb.isKinematic = true;
+            ballVel = p.GetVel();
+            x = CalcX(ballRb.mass, ballVel.magnitude);
+            ballRb.velocity = CalcVel(x, ballRb.mass);
+            MakeBallDynamic();
         }
-	//Rajal -Researched on how to make the ball bounce when it comes in contact with a spring
-		/*
-		void OnCollisionEnter2D(Collision2D c){
-		Rigidbody2D rBody = collisionInfo.collider.GetComponent<Rigidbody2D>();
-		Vector2 vel = rBody.velocity;
-       		float mag = vel.magnitude;
-        	rBody.AddForce(Vector2.Reflect(vel, collisionInfo.contacts[0].normal) * mag, ForceMode2D.Impulse);
-		}
-		*/
-	    //Make sure to uncheck the BoxCollider2D 
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        // Set the ball's rigidbody to null so we do not accidentally apply physics to the ball once it has
+        // left contact with the spring.
+        ballRb = null;
+    }
+
+    private void OnMouseDrag() {
+        dragging = true;
+        // Calculate the spring compress from the magnitude of the vector between initial and current mouse positions
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint);
+        Vector3 heading = cursorPosition - initialPos;
+        x = CalcX(draggingMass, heading.magnitude);
+    }
+
+    private void OnMouseUp() {
+        dragging = false;
+        // When the use lets go of the mouse button, calculate the ball's launch velocity
+        // from the x derived from OnMouseDrag()
+        if (ballRb != null) {
+            ballRb.velocity = CalcVel(x, draggingMass);
+            MakeBallDynamic();
+        }
+    }
+
+    private void MakeBallDynamic() {
+        if (ballRb.isKinematic) {
+            ballRb.isKinematic = false;
+        }
+    }
+
+    // Conservation of Energy equations
+
+    private float CalcX(float mass, float vel) {
+        return Mathf.Sqrt(mass * (vel * vel) / k);
+    }
+
+    private Vector3 CalcVel(float x, float mass) {
+        return transform.up.normalized * Mathf.Sqrt(k * (x * x) / mass);
     }
 }
 
