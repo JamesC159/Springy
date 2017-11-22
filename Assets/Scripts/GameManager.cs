76d5477b74841a8edcba4b38c7199338bb53ea58
Joey <joey@Joeys-iMac.local>
2017-11-22 10:13:59 -0600

    Simple implementation of columns spawning in GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public Collider[] colliders;
+	public GameObject column;
+	public int numColumns;
+	public float radius = 0f;
+
+	private int numInstantiated = 0;
+	private GameObject[] cols;
+
+	// Use this for initialization
+	void Start () {
+		float x = Random.Range (3, 17);
+		float y = Random.Range (-5, 2);
+		for(int i = 0; i < numColumns; i++){
+			GameObject obj = Instantiate (column, new Vector3 (x, y, 0), Quaternion.identity);
+			colliders = Physics.OverlapSphere (obj.transform.position, radius);
+			obj.SetActive (true);
+			x = Random.Range (3, 17);
+			y = Random.Range (-5, 2);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate() {
+		
+	}
+}
