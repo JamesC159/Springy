5f73c48f10e9fc879b4214acdf85e276764254c0
James Combs <vaze75159@gmail.com>
2017-11-24 22:27:32 -0600

    Improvements on sky spring instationation location and rotation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour {
 	public float maxY;
 	public float minSpringSpawnRotation;
 	public float maxSpringSpawnRotation;
+	public float angleDelta;
 
 	[HideInInspector]
 	public bool didWin = false;
@@ -38,36 +39,34 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float x = Random.Range (minX, maxX);
-		float y = Random.Range (minY, maxY);
 		float angle = 0f;
+		float delta = 0f;
 		GameObject obj = null;
+		Vector3 location = Vector3.zero;
 		Quaternion rotation = Quaternion.identity;
-		while (numInstantiated < numColumns) {
-			obj = Instantiate (column, new Vector3 (x, y, 0), Quaternion.identity);
-//			colliders = Physics2D.OverlapCircle (obj.transform.position, radius);
-//			if (colliders != null) {
-//				Destroy (obj);
-//			} else {
-//				numInstantiated++;
-//				obj.SetActive (true);
-//			}
-			numInstantiated++;
-			obj.SetActive (true);
-			x = Random.Range (minX, maxX);
-			y = Random.Range (minY, maxY);
+		for(int i = 0; i < numColumns; i++) {
+			location = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
+			obj = Instantiate (column, location, rotation);
+			obj.SetActive(true);
 		}
 		for (int i = 0; i < numSprings; i++) {
-			x = Random.Range (minX, maxX);
-			y = Random.Range (4, 7);
-			Vector3 location = new Vector3 (x, y, 0);
+			location = new Vector3 (Random.Range (minX, maxX), Random.Range (4, 7), 0);
 			Vector3 heading = location - fieldOrigin.transform.position;
 			if (heading.x < fieldOrigin.transform.position.x) {
 				// Restrict the magnitude of the distance from the field origin by 1/3
 			}
-			obj = Instantiate (spring, heading, rotation);
+			obj = Instantiate (spring, location, rotation);
 			// Find the angle between the field origin's up vector and the proposed spring location
-			obj.transform.Rotate(new Vector3(0f, 0, Vector3.SignedAngle(heading, fieldOrigin.transform.up, Vector3.forward)));
+			print(Vector3.SignedAngle(fieldOrigin.transform.up, heading, Vector3.forward));
+			if((int)Random.Range(0, 1000000000) % 2 == 0) {
+				if((int)Random.Range(0, 1000000000) % 2 == 0) {
+					delta = angleDelta * -1f;
+				}
+				obj.transform.Rotate(new Vector3(0f, 0, Vector3.SignedAngle(fieldOrigin.transform.up, heading, Vector3.forward) + delta));
+
+			} else {
+				obj.transform.Rotate(new Vector3(0f, 0, Vector3.SignedAngle(fieldOrigin.transform.up, heading, Vector3.forward) + delta));
+			}
 			obj.SetActive (true);
 		}
 	}
