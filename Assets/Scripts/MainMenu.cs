93964be225dd47f516adf2dddb944445ade329da
SarahiZazueta <sarahi_2803@hotmail.com>
2017-11-29 15:55:26 -0600

    Add files via upload

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Play () {
+        SceneManager.LoadScene(1);
+	}
+	
+	// Update is called once per frame
+	public void Quit () {
+        Application.Quit();
+		
+	}
+}
